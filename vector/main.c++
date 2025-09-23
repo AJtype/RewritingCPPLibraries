8c2259ade2e51bd01baefee1e1cf4a5e8d5dfc9b
Amit Judes <amit@judes.co.il>
2025-09-23 16:04:49 +0300

    created class vector with some constructor signature

--- vector/main.c++
+++ vector/main.c++
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "vector.hpp"
 
 int main() {
     return 0;
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -1,5 +1,17 @@
 #pragma once
 
-template<typename T1, typename T2>
-class vector {
+template<typename T>
+class vector { // TODO: add type specific vector for bool to optimize space efficiency
+private:
+    unsigned int typeSize;
+    unsigned int arrSize;
+    T* arr;
+
+public:
+    explicit vector(const unsigned int& count); // TODO
+    vector(const unsigned int& count, const T& value);
+    vector(const vector& other) = default;
+    vector(vector&& other) = default;
+    template<typename ... Args>
+    vector(Args&& ... args);
 };
