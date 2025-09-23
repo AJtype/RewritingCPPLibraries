4244f81faed74e0d88586d4e51902d5bc0aa5c16
Amit Judes <amit@judes.co.il>
2025-09-23 12:58:20 +0300

    after chatgpt review

--- pair/main.c++
+++ pair/main.c++
@@ -1,5 +1,6 @@
 #include "pair.hpp"
 #include <iostream>
+#include <string>
 
 int main() {
     pair<int, int> withNumbers(1, 2);
@@ -20,6 +21,11 @@ int main() {
     std::cout << "\nswapped withoutVars and copy" << std::endl;
     std::cout << "copy = " << copy.first << ", " << copy.second << std::endl;
     std::cout << "withoutVars = " << withoutVars.first << ", " << withoutVars.second << std::endl;
+
+    pair<int, std::string> original(1, "Hello");
+    pair<int, std::string> movedPair(std::move(original));
+    std::cout << "\nmovedPair = " << movedPair.first << ", " << movedPair.second << std::endl;
+    std::cout << "original after move = " << original.first << ", " << original.second << std::endl;
 
     return 0;
 }
--- pair/pair.hpp
+++ pair/pair.hpp
@@ -9,36 +9,38 @@ public:
 
     // constructor declarations
     pair();
-    pair(const T1& x, const T2& y);
+    explicit pair(const T1& x, const T2& y);
     pair(const pair&) = default;
-    pair(pair&&) = default; // TODO: untested
+    pair(pair&&) = default;
     pair<T1, T2>& operator=(const pair& other);
 
     // funcs
-    void swap(pair& other);
+    void swap(pair& other) noexcept;
 private:
 };
 
 // funcs
 // Default constructor
 template <typename T1, typename T2>
-inline pair<T1, T2>::pair() : first(T1()), second(T1()) {}
+inline pair<T1, T2>::pair() : first(T1()), second(T2()) {}
 
 // Two-argument constructor
-template<typename T1, typename T2> // TODO: move to c++ file
+template<typename T1, typename T2>
 pair<T1, T2>::pair(const T1& x, const T2& y)
     : first(x), second(y) {}
 
 template <typename T1, typename T2>
 inline pair<T1, T2>& pair<T1, T2>::operator=(const pair& other) {
-    first = other.first;
-    second = other.second;
+    if (this != &other) {
+        first = other.first;
+        second = other.second;
+    }
 
     return *this;
 }
 
 template <typename T1, typename T2>
-inline void pair<T1, T2>::swap(pair& other)
+inline void pair<T1, T2>::swap(pair& other) noexcept
 {
     std::swap(first, other.first); // TODO: replace with my swap
     std::swap(second, other.second);
