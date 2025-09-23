c5a0a3bb39d12135f11481127de06071f491c920
Amit Judes <amit@judes.co.il>
2025-09-23 17:05:40 +0300

    added some empty funcs

--- vector/main.c++
+++ vector/main.c++
@@ -2,12 +2,17 @@
 #include "vector.hpp"
 
 int main() {
-    vector<int> vec(5, 2);
+    vector<int> vec(5);
+
+    for (size_t i = 0; i < 5; i++)
+    {
+        vec[i] = i;
+    }
 
     std::cout << "vec = {";
     for (size_t i = 0; i < 5; i++)
     {
-        std::cout << vec.arr[i] << ", ";
+        std::cout << vec[i] << ", ";
     } std::cout << "}" << std::endl;
     
 
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -3,24 +3,41 @@
 template<typename T>
 class vector { // TODO: add type specific vector for bool to optimize space efficiency
 private:
-    unsigned int typeSize;
-    unsigned int arrSize;
+    size_t typeSize;
+    size_t arrSize;
     T* arr;
 
 public:
-    explicit vector(const unsigned int& count); // TODO
+    // Constructors
+    vector() = default;
+    explicit vector(const size_t& count); // TODO
     vector(size_t count, const T& value);
-    vector(const vector& other) = default;
-    vector(vector&& other) = default;
-    template<typename ... Args>
-    vector(Args&& ... args);
+    vector(const vector& other) = default; // copy constructor
+    vector(vector&& other) = default; // move constructor
+    template<typename ... Args> vector(Args&& ... args); // uniform constructor
+
+    // Getters & Setters
+    T& operator[](const int pos);
 };
 
 template <typename T>
-inline vector<T>::vector(size_t count, const T& value) : arr(new T[count]), arrSize(count), typeSize(sizeof(T))
+inline vector<T>::vector(const size_t &count) : arr(new T[count]) {}
+
+template <typename T>
+inline vector<T>::vector(size_t count, const T &value)
+    : arr(new T[count]), arrSize(count), typeSize(sizeof(T))
 {
     for (size_t i = 0; i < count; i++)
     {
         arr[i] = value;
     }
+}
+
+template <typename T>
+template <typename... Args>
+inline vector<T>::vector(Args &&...args) {}
+
+template <typename T>
+inline T& vector<T>::operator[](const int pos) {
+    return arr[pos];
 }
