9629b293eaaa0362670cf09f94b8667d2a682b80
Amit Judes <amit@judes.co.il>
2025-09-24 17:25:47 +0300

    added funcs emplace_back and operator[]

--- list/list.hpp
+++ list/list.hpp
@@ -3,12 +3,12 @@
 
 template<typename T>
 class list{ // doubly linked list
-private:
+public:
     struct node
     {
         T value;
-        T* next;
-        T* prev;
+        node* next;
+        node* prev;
     };
     
     node* head;
@@ -18,16 +18,60 @@ private:
 public:
     // constructor declarations
     list();
-    list(const list&) = default;
-    list(list&&) = default;
+    list(const list&) = default; // TODO: untested
+    list(list&&) = default; // TODO: untested
+    ~list() = default; // TODO: remove all the allocated memory
     list<T>& operator=(const list& other);
 
+    // getter setters
+    void emplace_back(const T& value); // TODO: replace with Args&&... args
+    T& operator[](const int& pos); // TODO: untested
+
     // funcs
+    bool empty(); // TODO: unfinished
     void swap(list& other) noexcept;
 };
 
-// funcs
-
+// constructors
+// default constructor
 template <typename T>
 inline list<T>::list() :
     head(nullptr), end(nullptr), size(0) {}
+
+template <typename T>
+inline void list<T>::emplace_back(const T &value)
+{
+    if (empty()) {
+        // std::cout << "emplace back empty" << std::endl;
+        head = new node();
+        head->value = value;
+        head->next = nullptr;
+        head->prev = nullptr;
+        end = head;
+    } else {
+        // std::cout << "emplace back not empty" << std::endl;
+        end->next = new node();
+        head->value = value;
+        head->next = end;
+        head->prev = nullptr;
+        end = end->next;
+    } size++;
+}
+
+template <typename T>
+inline T &list<T>::operator[](const int &pos)
+{
+    node* curr = head;
+    
+    for (unsigned int i = 0; i < pos; i++) {
+        curr = curr->next;
+    }
+    
+    return curr->value;
+}
+
+template <typename T>
+inline bool list<T>::empty()
+{
+    return true;
+}
--- list/main.c++
+++ list/main.c++
@@ -4,5 +4,13 @@
 int main() {
     list<int> l;
 
+    l.emplace_back(2);
+
+    std::cout << "list = {";
+    for (size_t i = 0; i < 1; i++)
+    {
+        std::cout << l[i] << ", ";
+    } std::cout << "}" << std::endl;
+
     return 0;
 }
